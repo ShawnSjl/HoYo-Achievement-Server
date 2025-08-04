b2a6e974c7506ea42ec7adb95cfcf47e66973b39
ShawnSjl <sjlshawn@gmail.com>
2025-08-04 13:00:35 +0800

    init commit

---
+++ src/main/java/tech/sjiale/hoyo_achievement_server/HoYoAchievementServerApplication.java
@@ -0,0 +1,13 @@
+package tech.sjiale.hoyo_achievement_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoYoAchievementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoYoAchievementServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/sjiale/hoyo_achievement_server/HoYoAchievementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sjiale.hoyo_achievement_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoYoAchievementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
