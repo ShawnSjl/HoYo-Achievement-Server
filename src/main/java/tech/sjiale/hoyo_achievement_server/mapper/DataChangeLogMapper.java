1655fad8946ca4aa5cf521e2351520501e74fe18
ShawnSjl <sjlshawn@gmail.com>
2026-05-30 23:16:06 -0400

    refactor: remove unused DataChangeLog table

--- src/main/java/tech/sjiale/hoyo_achievement_server/mapper/DataChangeLogMapper.java
+++
@@ -1,9 +0,0 @@
-package tech.sjiale.hoyo_achievement_server.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import tech.sjiale.hoyo_achievement_server.entity.DataChangeLog;
-
-@Mapper
-public interface DataChangeLogMapper extends BaseMapper<DataChangeLog> {
-}
